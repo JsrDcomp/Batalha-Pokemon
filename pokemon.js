430caef1c41e6f90209ca6ea8351f2ab0e4c9a63
Tasso Marcel <tassomarcel87@gmail.com>
2023-09-30 21:57:48 -0300

    transformei os ataques do oponente em uma unica função attackOp

--- pokemon.js
+++ pokemon.js
@@ -1,148 +1,93 @@
 var userHP = 100;
 var opHP = 100;
-opAttacks = [flameThrower, dragonClaw, ember, growl];
-playerMove = 0;
-/* users moves */
+
+let playerMove = 0;
+/* movimentos do usuário */
 
 /* ~ Troquei as funções dos ataques do jogador para constantes e assim reutilizar o código na função atack */
-const waterCannon = () => atack(1)
-const  waterPulse = () => atack(2)
-const surf = () => atack(3)
-const tackle = () => atack(4)
+const waterCannon = () => attackUser(1)
+const  waterPulse = () => attackUser(2)
+const surf = () => attackUser(3)
+const tackle = () => attackUser(4)
+
+const flameThrower = () => attackOp(1)
+const dragonClaw = () => attackOp(2)
+const ember = () => attackOp(3)
+const growl = () => attackOp(4)
 
 
-const atack = (damage) => {
+// função que faz o ataque do usario com o dano passado como argumento
+const attackUser = (damage) => {
   if(playerMove == 0 && userHP != 0) {
-    var miss = Math.floor((Math.random() * 10) + 1); // miss rate
+    const miss = Math.floor((Math.random() * 10) + 1); // chance de erro
     if(miss == 1) {
       document.getElementById('message').innerHTML = " Blastoise's attack missed! ";
     }
     else {
-      document.getElementById('message').innerHTML = " Blastoise used water cannon "; // attack
-      var critical = Math.floor((Math.random() * 10) + 1); // critical
+      document.getElementById('message').innerHTML = " Blastoise used water cannon "; // ataque
+      const critical = Math.floor((Math.random() * 10) + 1); // chanche de critico
       if(critical == 4){
-        for(var x = 0; x < 2; x++){
-          opHP = opHP - damage; // yes critical
-        }
+        // Estou criando esse Array aplicando um map que subtrai do hp inimigo o dano para substuir o loop for para garantir o parádigma funcional do código
+        Array.from({ length:2 }).map(() => {{
+          opHP = opHP - damage // o arrai aqui cria um registro e aplica a função do map duas vezes devido ao length, se o ataque for um critico 
+      }});
       }
       else{
-        opHP = opHP - damage; // no critical
+        opHP = opHP - damage; // sem critico
       }
       if(opHP < 0){ opHP = 0} //faint
-        document.getElementById('apHP').innerHTML = opHP; // update hp
+        document.getElementById('apHP').innerHTML = opHP; // atualiza o hp
       if(opHP == 0){
-        document.getElementById('message').innerHTML = " Charizard fainted! " // update message
+        document.getElementById('message').innerHTML = " Charizard fainted! " // atualiza a mensagem
       }
     }
-    //wait();
-    playerMove = 1; // update player move
+    //espera();
+    playerMove = 1; // atualiza o movimento do jogador
 }
 }
 
-/* opponent's moves */
+/* movimentos dos oponentes */
 
-function flameThrower() {
-  var miss = Math.floor((Math.random() * 10) + 1); // miss rate
+// função que faz o ataque do oponente com o dano passado como argumento
+const attackOp = (damage) => {
+  const miss = Math.floor((Math.random() * 10) + 1); // chance de erro
   if(miss == 1 ) {
-  document.getElementById('message').innerHTML = " Charizard's attack missed! " // attack missed
+  document.getElementById('message').innerHTML = " Charizard's attack missed! " // errou o ataque
   }
   else {
-  document.getElementById('message').innerHTML = " Charizard used flame thrower " // attack
-    var critical = Math.floor((Math.random() * 10) + 1); // critical
+  document.getElementById('message').innerHTML = " Charizard used flame thrower " // ataque
+    const critical = Math.floor((Math.random() * 10) + 1); // critico
       if(critical == 4){
-        for(var x = 0; x < 2; x++){ // yes critical
-          userHP = userHP - 30;
-        }
+        // Estou criando esse Array aplicando um map que subtrai do hp usário o dano para substuir o loop for para garantir o parádigma funcional do código
+        Array.from({ length:2 }).map(() => {{
+          userHP = userHP - damage // o arrai aqui cria um registro e aplica a função do map duas vezes devido ao length, se o ataque for um critico 
+      }});
       }
       else{
-        userHP = userHP - 30; // no critical
+        userHP = userHP - damage
       }
-  if(userHP < 0) { userHP = 0} // faint
-  document.getElementById('myHP').innerHTML = userHP; // update hp
-    if(userHP == 0) { // fainted
-      document.getElementById('message').innerHTML = " Blastoise fainted! " // fainted
-    }
-  }
-}
-
-function dragonClaw() {
-  var miss = Math.floor((Math.random() * 10) + 1);
-  if(miss == 1 ) {
-    document.getElementById('message').innerHTML = " Charizard's attack missed! "
-  }
-  else {
-  document.getElementById('message').innerHTML = " Charizard used dragon claw "
-  var critical = Math.floor((Math.random() * 10) + 1);
-    if(critical == 4){
-      for(var x = 0; x < 2; x++){
-        userHP = userHP - 20;
-      }
-    }
-    else{
-      userHP = userHP - 20;
-    }
-  if(userHP < 0) { userHP = 0}
-  document.getElementById('myHP').innerHTML = userHP;
-    if(userHP == 0){
-      document.getElementById('message').innerHTML = " Blastoise fainted! "
-    }
-  }
-}
-
-function ember() {
-  var miss = Math.floor((Math.random() * 10) + 1);
-  if(miss == 1 ) {
-    document.getElementById('message').innerHTML = " Charizard's attack missed! "
-  }
-  else {
-  document.getElementById('message').innerHTML = " Charizard used ember "
-  var critical = Math.floor((Math.random() * 10) + 1);
-    if(critical == 4){
-      for(var x = 0; x < 2; x++){
-        userHP = userHP - 10;
-      }
-    }
-    else{
-      userHP = userHP - 10;
-    }
-  if(userHP < 0) { userHP = 0}
-  document.getElementById('myHP').innerHTML = userHP;
-    if(userHP == 0){
-      document.getElementById('message').innerHTML = " Blastoise fainted! "
-    }
-  }
-}
-
-function growl() {
-  var miss = Math.floor((Math.random() * 10) + 1);
-  if(miss == 1 ) {
-    document.getElementById('message').innerHTML = " Charizard's attack missed! "
-  }
-  else {
-  document.getElementById('message').innerHTML = " Charizard used growl "
-  var critical = Math.floor((Math.random() * 10) + 1);
-    if(critical == 4){
-      for(var x = 0; x < 2; x++){
-        userHP = userHP - 5;
-      }
-    }
-    else{
-      userHP = userHP - 5;
-    }
-  if(userHP < 0) { userHP = 0}
-  document.getElementById('myHP').innerHTML = userHP;
-    if(userHP == 0){
-      document.getElementById('message').innerHTML = " Blastoise fainted! "
+  if(userHP < 0) { userHP = 0} // desmaio
+  document.getElementById('myHP').innerHTML = userHP; // atualiza o hp
+    if(userHP == 0) { // desmaiado
+      document.getElementById('message').innerHTML = " Blastoise fainted! " // desmaiado
     }
   }
 }
 
 
-
-function compPokemon() { // continue
-  if(playerMove == 1 && opHP != 0) { // whos move
-  var move = Math.floor((Math.random() * 4) + 1); // choose move randomly
-    opAttacks[move](); // call attack from array
-    playerMove = 0; // update player move
+// função que seleciona os ataques do inimigo de forma aleatória
+const opAttack = () => { // continue
+  if(playerMove == 1 && opHP != 0) { 
+  var move = Math.floor((Math.random() * 4) + 1); // escolhe um numero inteiro entre 1 e 4, e dependendo do numero, irá fazer um ataque aleatório
+    if(move == 1){
+      flameThrower()
+    }else if(move==2){
+      dragonClaw()
+    }else if(move==3){
+      ember()
+    }else{
+      growl()
+    } 
+    playerMove = 0; // atualiza a vez de quem joga
   }
 }
